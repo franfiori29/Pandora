c7b179cf866895e0f5d0720de481f64467895748
franfiori29 <franfiori@gmail.com>
2021-01-28 03:30:31 -0300

    login n register styles

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyCS3JarffdIZ-rJW3qRE8Mc3-zMkCtglm8",
+	authDomain: "signal-clone-build-95afe.firebaseapp.com",
+	projectId: "signal-clone-build-95afe",
+	storageBucket: "signal-clone-build-95afe.appspot.com",
+	messagingSenderId: "800113029844",
+	appId: "1:800113029844:web:3504201c0f29e418764141",
+};
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,81 @@
+import React, { useState, useLayoutEffect } from "react";
+import { StyleSheet, View, KeyboardAvoidingView } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Button, Input, Text } from "react-native-elements";
+
+const RegisterScreen = ({ navigation }) => {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [imageUrl, setImageUrl] = useState("");
+
+	useLayoutEffect(() => {
+		navigation.setOptions({
+			headerBackTitle: "Back to Login",
+		});
+	}, [navigation]);
+
+	const register = () => {};
+
+	return (
+		<KeyboardAvoidingView behavior='padding' style={styles.container}>
+			<StatusBar style='light' />
+			<Text h3 style={{ marginBottom: 50 }}>
+				Create an account
+			</Text>
+			<View style={styles.inputContainer}>
+				<Input
+					placeholder='Full name'
+					autoFocus
+					tye='text'
+					value={name}
+					onChangeText={setName}
+				/>
+				<Input
+					placeholder='Email'
+					tye='email'
+					value={email}
+					onChangeText={setEmail}
+				/>
+				<Input
+					placeholder='Password'
+					tye='password'
+					secureTextEntry
+					value={password}
+					onChangeText={setPassword}
+				/>
+				<Input
+					placeholder='Profile Pic Url (optional)'
+					tye='text'
+					value={imageUrl}
+					onChangeText={setImageUrl}
+					onSubmitEditing={register}
+				/>
+			</View>
+			<Button
+				containerStyle={styles.button}
+				title='Register'
+				onPress={register}
+			/>
+		</KeyboardAvoidingView>
+	);
+};
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		padding: 10,
+		backgroundColor: "white",
+	},
+	button: {
+		width: 200,
+		marginTop: 10,
+	},
+	inputContainer: {
+		width: 300,
+	},
+});
